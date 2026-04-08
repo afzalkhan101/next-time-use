8b00537594bcc60ccacf07f989dfd9f4a266865e
afzalkhan101 <afzalkhan01765@gmail.com>
2026-04-08 12:03:11 +0600

    updated code

--- salesperson_live_tracking/models/res_users.py
+++ salesperson_live_tracking/models/res_users.py
@@ -1,15 +1,12 @@
 from odoo import fields, models
 
-
 class ResUsers(models.Model):
     _inherit = "res.users"
-
     salesperson_tracker_id = fields.One2many(
         "salesperson.tracker",
         "user_id",
         string="Salesperson Tracker",
     )
-
     def _ensure_salesperson_tracker(self):
         self.ensure_one()
         tracker = self.env["salesperson.tracker"].sudo().search([("user_id", "=", self.id)], limit=1)
